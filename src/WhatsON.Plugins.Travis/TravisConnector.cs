4d6a83a7de735a4d3a0d8625fa1e6e6c97074ef1
Steffen Wilke <mail@steffen-wilke.de>
2019-08-27 20:14:19 +0200

    Set duration for the build status.
    Use new version of the WhatsON NuGet.

--- src/WhatsON.Plugins.Travis/TravisConnector.cs
+++ src/WhatsON.Plugins.Travis/TravisConnector.cs
@@ -50,8 +50,6 @@ namespace WhatsON.Plugins.Travis
 
       var latestBuild = await TravisAPI.GetBuild(job.LastBuild.Id);
       this.CurrentStatus = CreateStatus(latestBuild);
-
-      this.CurrentStatus = CreateStatus(latestBuild);
       if (this.Snapshots.Count == 0 && job.LastBuild.BuildNumber > 1)
       {
         foreach (var build in await TravisAPI.GetBuilds(this.Owner, this.Repository, this.Branch, MaxSnapshots))
@@ -70,7 +68,8 @@ namespace WhatsON.Plugins.Travis
         Building = build.Finished == default,
         State = build.GetState(),
         BuildNumber = build.BuildNumber,
-        Time = build.Started
+        Time = build.Started,
+        Duration = new TimeSpan(0, 0, build.Duration),
       };
 
       return status;
